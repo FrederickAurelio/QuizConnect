06c8b64c1073eeef533aca87bc34dad74d8feb29
Frederick Aurelio Halim <frederick.ah88@gmail.com>
2026-01-11 15:03:02 +0800

    chore: use better tailwind class naem for some component

--- frontend/src/components/ui/tooltip.tsx
+++ frontend/src/components/ui/tooltip.tsx
@@ -54,7 +54,7 @@ function TooltipContent({
       >
         {children}
         {!removeArrow && (
-          <TooltipPrimitive.Arrow className="bg-foreground fill-foreground z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px]" />
+          <TooltipPrimitive.Arrow className="bg-foreground fill-foreground z-50 size-2.5 translate-y-[calc(-50%-2px)] rotate-45 rounded-xs" />
         )}
       </TooltipPrimitive.Content>
     </TooltipPrimitive.Portal>
